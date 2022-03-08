0e223d761c4618a3bbbbfa12e0b1f324fb003cdc
cccs-rs <ryan.samaroo@cyber.gc.ca>
2022-03-08 12:31:25 +0000

    Setup to use common OCR lib

--- Dockerfile
+++ Dockerfile
@@ -6,14 +6,10 @@ ENV SERVICE_PATH document_preview.document_preview.DocumentPreview
 USER root
 
 RUN mkdir -p /usr/share/man/man1mkdir -p /usr/share/man/man1
-RUN apt-get update && apt-get install -y wget
+RUN apt-get update && apt-get install -y wget tesseract-ocr
 RUN wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.buster_amd64.deb
 RUN apt-get install -y poppler-utils libreoffice  ./wkhtmltox_0.12.6-1.buster_amd64.deb --no-install-recommends
-RUN pip install pdf2image Pillow natsort imgkit compoundfiles compressed_rtf
-
-# Install Image/Science libraries for Python & Tesseract OCR engine/ Language plug-ins
-RUN apt-get install -y libjpeg-dev zlib1g-dev imagemagick tesseract-ocr && rm -rf /var/lib/apt/lists/*
-RUN pip install numpy scipy matplotlib pytesseract
+RUN pip install pdf2image Pillow natsort imgkit compoundfiles compressed_rtf pytesseract
 
 USER assemblyline
 
--- document_preview/document_preview.py
+++ document_preview/document_preview.py
@@ -1,45 +1,16 @@
 import json
 import os
-import pytesseract
-import re
-import shutil
 import subprocess
 
 from natsort import natsorted
 from pdf2image import convert_from_path
-from PIL import Image
 
 from assemblyline_v4_service.common.base import ServiceBase
 from assemblyline_v4_service.common.result import BODY_FORMAT, Result, ResultImageSection, ResultSection, Heuristic
+from assemblyline_v4_service.common.extractor.ocr import ocr_detections
 
 from document_preview.helper.emlrender import processEml as eml2image
 from document_preview.helper.outlookmsgfile import load as msg2eml
-
-# TODO: Would prefer this mapping to be dynamic from trusted sources (ie. import from library), but will copy-paste for now
-INDICATORS_MAPPING = {
-    ('ransomware', 1):   re.compile('|'.join([
-        # https://github.com/cuckoosandbox/community/blob/master/modules/signatures/windows/ransomware_message.py
-        "your files", "your data", "your documents", "restore files",
-        "restore data", "restore the files", "restore the data", "recover files",
-        "recover data", "recover the files", "recover the data", "has been locked",
-        "pay fine", "pay a fine", "pay the fine", "decrypt", "encrypt",
-        "recover files", "recover data", "recover them", "recover your",
-        "recover personal", "bitcoin", "secret server", "secret internet server",
-        "install tor", "download tor", "tor browser", "tor gateway",
-        "tor-browser", "tor-gateway", "torbrowser", "torgateway", "torproject.org",
-        "ransom", "bootkit", "rootkit", "payment", "victim", "AES128", "AES256",
-        "AES 128", "AES 256", "AES-128", "AES-256", "RSA1024", "RSA2048",
-        "RSA4096", "RSA 1024", "RSA 2048", "RSA 4096", "RSA-1024", "RSA-2048",
-        "RSA-4096", "private key", "personal key", "your code", "private code",
-        "personal code", "enter code", "your key", "unique key"
-    ])),
-    ('macros', 2): re.compile('|'.join([
-        # https://github.com/cuckoosandbox/community/blob/17d57d46ccbca0327a8299cb93abba8604b74df7/modules/signatures/windows/office_enablecontent_ocr.py
-        "enable macro",
-        "enable content",
-        "enable editing",
-    ]))
-}
 
 
 class DocumentPreview(ServiceBase):
@@ -88,53 +59,42 @@ class DocumentPreview(ServiceBase):
             # Render EML as PNG
             eml2image(file_contents, self.working_directory, self.log)
 
-        # Images don't required to be rendered, however could still be useful for OCR analysis
-        elif file_type.startswith('image'):
-            shutil.move(file, os.path.join(self.working_directory, 'output_0'))
-
     def execute(self, request):
         result = Result()
 
         # Attempt to render documents given and dump them to the working directory
         self.render_documents(request.file_type, request.file_path, request.file_contents)
+        max_pages = request.get_param('max_pages_rendered')
         images = list()
 
         # Create an image gallery section to show the renderings
         if any("output" in s for s in os.listdir(self.working_directory)):
-            image_section = ResultImageSection(request, "Successfully extracted the preview.")
-
-            i = 0
             previews = [s for s in os.listdir(self.working_directory) if "output" in s]
-            for preview in natsorted(previews):
+            total_pages = len(previews)
+            image_section = ResultImageSection(request,
+                                               "Successfully extracted the preview. "
+                                               f"Displaying {min(max_pages, total_pages)} of {total_pages}.")
+            for i, preview in enumerate(natsorted(previews)):
+                if i >= max_pages:
+                    break
                 image_path = f"{self.working_directory}/{preview}"
                 images.append(image_path)
                 title = f"preview_{i}.jpeg"
                 desc = f"Here's the preview for page {i}"
                 image_section.add_image(image_path, title, desc)
-                i += 1
 
             result.add_section(image_section)
 
         # Proceed with analysis of output images
-        for image_path in images:
-            ocr_output = ''
-            with Image.open(image_path) as img:
-                ocr_output = pytesseract.image_to_string(img)
-            parent = ResultSection(f'OCR Analyis on {os.path.basename(image_path)}')
-            for indicator, regex_exp in INDICATORS_MAPPING.items():
-                search_results = regex_exp.findall(ocr_output) + regex_exp.findall(ocr_output.lower())
-                if search_results:
-                    self.log.info(f'Found {indicator[0]}')
-                    body = {
-                        term: [line for line in ocr_output.split('\n') + ocr_output.lower().split('\n') if term in line]
-                        for term in set(search_results)}
-                    ResultSection(
-                        f'OCR Detection: {indicator[0]}', body=json.dumps(body),
-                        body_format=BODY_FORMAT.JSON,
-                        heuristic=Heuristic(heur_id=indicator[1],
-                                            frequency=len(search_results)),
-                        parent=parent)
-            if parent.subsections:
-                result.add_section(parent)
+        for i, image_path in enumerate(images):
+            if i >= max_pages:
+                break
 
+            detections = ocr_detections(image_path)
+            if any(v for v in detections.values()):
+                result.add_section(
+                    ResultSection(f'OCR Analysis on {os.path.basename(image_path)}',
+                                  body=json.dumps(detections), body_format=BODY_FORMAT.JSON,
+                                  heuristic=Heuristic(1, signatures={k: len(v) for k, v in detections.items()}))
+                )
         request.result = result
